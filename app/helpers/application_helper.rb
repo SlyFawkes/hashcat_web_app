1e663452fa82cec8d22f49e1681dfe0f5487bdc0
dan <deanwelch286@gmail.com>
2025-03-23 19:02:52 +0000

    Rubocop all the things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Generated Application Helper
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Generated Application Record
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
